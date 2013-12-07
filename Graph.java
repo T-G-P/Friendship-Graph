269a360cd0327f4b81eb40e1d85e361cee081d04
Tobias Perelstein <tobias.perelstein@gmail.com>
2013-12-07 01:17:39 -0500

    Build Graph is finished.

--- Graph.java
+++ Graph.java
@@ -15,37 +15,56 @@ public class Graph {
 
     	int numPeople = Integer.parseInt(sc.nextLine());
     	graph = new HashMap<String, Node>();
-    	while(graph.size() <= numPeople && sc.hasNextLine()){
+    	while(graph.size() < numPeople && sc.hasNextLine()){
     		Node temp = new Node(makeUser(sc.nextLine()), null);
     		graph.put(temp.data.name, temp);
+    		//System.out.println(graph.size());
     	}
     	while(sc.hasNextLine()){
-    		String line = sc.nextLine();
-    		line.toLowerCase();
-    		Scanner lineSc = new Scanner(line).useDelimiter("\\s*\\|\\s*");
-        	Node firstFriend = graph.get(lineSc.next());
-        	Node secondFriend = new Node(graph.get(sc.next()).data, null);
-    		makeFriendships(firstFriend, secondFriend);
-    		
+            String line = sc.nextLine();
+            line.toLowerCase();
+            Scanner lineSc = new Scanner(line).useDelimiter("\\s*\\|\\s*");
+            Node firstFriend = graph.get(lineSc.next());
+            Node firstFriendCopy = new Node(firstFriend.data,null);
+            Node secondFriend = graph.get(lineSc.next());
+            Node secondFriendCopy = new Node(secondFriend.data,null);
+            makeFriendships(firstFriend, secondFriendCopy);
+            makeFriendships(secondFriend, firstFriendCopy);
+            
     	}
+    	
+    	printGraph(graph);
     	
     	
     }
     
+    private String printList(Node friend){
+    	Node ptr;
+    	String answer = "";
+    	for(ptr=friend; ptr!=null; ptr=ptr.next){
+    		
+    		if(ptr==friend){
+    			continue;
+    		}
+    		answer+=(ptr.data.name+" ");
+    	}
+    	return friend.data.name+" is friends with: "+answer;
+    }
+    
     private void printGraph(HashMap<String, Node> graph){
     	for(String name: graph.keySet()){
-    		System.out.println(graph.get(name).data.name);
+    		System.out.println(printList(graph.get(name)));
     	}
     	
     }
     
     private User makeUser(String line){
     	line.toLowerCase();
-    	Scanner sc = new Scanner(line).useDelimiter("\\s*\\|\\s*");
+    	Scanner Usersc = new Scanner(line).useDelimiter("\\s*\\|\\s*");
     	User person = new User(null, null);    	
-    	person.name = sc.next();
-    	if (sc.next().equals("y")){
-    		person.school = sc.next();
+    	person.name = Usersc.next();
+    	if (Usersc.next().equals("y")){
+    		person.school = Usersc.next();
     	}    	 
     	return person;
     	     	   	
